e9a581fa88ac360a009e6ec8bec757ca0fcb4684
jhon-RA <jandresra@gmail.com>
2024-11-05 18:32:01 -0500

    primer comit

---
+++ index.js
@@ -0,0 +1,67 @@
+// let number = '999999'
+// var number2 = '99999'
+
+// document.write('number')
+// console.log(`number: ${number}`)
+
+// if(number === number2){
+//  console.log('si es')
+// }else if(number === number2){
+//     console.log('tal vez')
+// }else{
+//     console.log('no es ')
+// }
+// for(let i = 10; i >= 1; i--) {
+//     console.log(i)
+// }
+//let sw = true
+// let cont = 0
+// while(sw == true){
+      
+//     cont = cont +1
+    
+//     if(cont == 100){
+//         sw = false
+//         console.log(cont)
+//       }
+// }
+
+// while (sw == true){
+//     console.log('HAS TU PEDIDO')
+//     console.log('RES')
+//     console.log('POLLOS')
+//     console.log('CERDO')
+//     console.log('')
+//     sw = false
+// }
+
+
+// let number1 = parseInt(prompt('Digite un numero'))
+// let number2 = parseInt(prompt('Digite un segundo numero'))
+
+// suma = number1 + number2
+// resta = number1 - number2
+// multiplicacion = number1 * number2
+
+// console.log(suma)
+// console.log(resta)
+// console.log(multiplicacion)
+
+// metodos en cadena
+// endswith final
+//startswith principio
+//include sirve para saver si una cadena esta en otra cadena y de vulve false o true
+//indexof para saber si una cadena esta en otra
+//padstrart rellena la cadena de carracteres y los llena
+// padend rellena la cadena con cualquier carracter pero al final
+// repeat repite la cadena la cantida de veces que le indiques 
+//split divide la cadena de acuerdo al valor que seleaccine
+// substring sive para identificar  desde donde quieres que se inicie 
+//toLowerCase transforma de mayuscula a miniscula
+//toUpperCase transforma de minuscula a mayuscula
+
+
+
+let nombre = 'andres'
+let apellido = 'reyes'
+document.write(nombre.toUpperCase)
